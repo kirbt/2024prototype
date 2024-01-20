f3f95a01d4e66872e1f941462e52f2871bbca80a
Jonah <58869582+jbko6@users.noreply.github.com>
2024-01-20 09:40:01 -0800

    gulp

---
+++ src/main/java/frc/robot/subsystems/CoolSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkFlex;
+
+public class CoolSubsystem extends SubsystemBase {
+
+    public static final double SPEED = 1;
+
+
+
+    public CoolSubsystem () {
+
+    }
+
+    
+}
